f213960bc43dc4c012265bbd17d6d38f78218188
atanana <tanana@email.ru>
2017-11-29 22:51:40 +0300

    init parse csv

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import * as React from 'react';
 import './App.css';
 import Uploader from './components/Uploader';
+import * as Papa from 'papaparse';
 
 class App extends React.Component {
+    onFileSelected(files: FileList | null) {
+        if (files && files.length) {
+            Papa.parse(files[0], {
+                encoding: 'cp1251',
+                complete: results => {
+                    console.log(results);
+                }
+            });
+        } else {
+            alert('Пожалуйста, выберите файл');
+        }
+    }
+
     render() {
         return (
             <div className="App container">
                 <div className="box">
                     <div className="content">Выберите файл с результатами</div>
-                    <Uploader/>
+                    <Uploader onFileSelected={this.onFileSelected}/>
                 </div>
             </div>
         );
--- src/components/Uploader.tsx
+++ src/components/Uploader.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 
-const Uploader = () => (
+const Uploader = ({onFileSelected}: { onFileSelected: (files: FileList | null) => void }) => (
     <div>
         <div className="file">
             <label className="file-label">
-                <input className="file-input" type="file" name="resume"/>
+                <input className="file-input" type="file" name="resume" onChange={e => onFileSelected(e.target.files)}/>
                 <span className="file-cta">
                     <span className="file-icon">
                         <i className="fa fa-upload"/>
