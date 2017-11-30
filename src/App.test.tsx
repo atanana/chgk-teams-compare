3fd967a6e6c226f3e6e8676b35a03411af6d92bb
atanana <tanana@email.ru>
2017-12-01 00:03:49 +0300

    app error tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,26 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
+import ResultsParser from './parse/ResultsParser';
+
+const Parser = jest.fn<ResultsParser>();
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+    const div = document.createElement('div');
+    ReactDOM.render(<App parser={new Parser}/>, div);
+});
+
+it('renders no error', () => {
+    const app = shallow(<App parser={new Parser}/>);
+    const errors = app.find('.notification.is-danger');
+    expect(errors).toHaveLength(0);
+});
+
+it('renders error', () => {
+    const app = shallow(<App parser={new Parser}/>);
+    app.setState({ error: 'test error' });
+    const errors = app.find('.notification.is-danger');
+    expect(errors).toHaveLength(1);
+    expect(errors.text()).toEqual('test error :(');
 });
