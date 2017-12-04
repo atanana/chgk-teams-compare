b1c58dfc30af99d4f020a20dad5e29c75f02abc3
atanana <tanana@email.ru>
2017-12-04 22:06:01 +0300

    results parser test

--- src/parse/ResultsParser.test.ts
+++ src/parse/ResultsParser.test.ts
@@ -2,6 +2,7 @@ import ResultsParser from './ResultsParser';
 import { PapaWrapper } from './PapaWrapper';
 import ParseResult = PapaParse.ParseResult;
 import ParseError = PapaParse.ParseError;
+import { TeamData, Tour } from '../data/ResultsData';
 
 const FileMock = jest.fn<File>();
 
@@ -36,4 +37,38 @@ it('throws an error when there are errors in result', () => {
     }));
     const parser = new ResultsParser(new PapaWrapperStub);
     return expect(parser.parse(new FileMock)).rejects.toMatchObject(new Error());
+});
+
+it('parse team data', () => {
+    const PapaWrapperStub = jest.fn<PapaWrapper>(() => ({
+        parseFile(): Promise<ParseResult> {
+            const result = new ParseResultMock;
+            result.errors = [];
+            result.data = [
+                [' Номер команды', 'Название', 'Город', 'Тур'],
+                ['49804', 'Борский корабел', 'Москва', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '0', '1'],
+                ['49804', 'Борский корабел', 'Москва', '2', '1', '1', '1', '1', '1', '1', '1', '1', '1', '0', '1'],
+                ['49804', 'Борский корабел', 'Москва', '3', '1', '1', '1', '1', '1', '1', '1', '1', '1', '0', '1'],
+                [' Номер команды', 'Название', 'Город', 'Тур'],
+                ['59865', 'Обниматься', 'Санкт-Петербург', '1', '1', '1', '0', '1', '1', '1', '1', '1', '1', '1', '0'],
+                ['59865', 'Обниматься', 'Санкт-Петербург', '2', '1', '1', '0', '1', '1', '1', '1', '1', '1', '1', '0'],
+                ['59865', 'Обниматься', 'Санкт-Петербург', '3', '1', '1', '0', '1', '1', '1', '1', '1', '1', '1', '0']
+            ];
+            return Promise.resolve(result);
+        }
+    }));
+    const parser = new ResultsParser(new PapaWrapperStub);
+    const teamData1 = new TeamData(49804, 'Борский корабел', 'Москва');
+    teamData1.tours = [
+        new Tour(1, [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1]),
+        new Tour(2, [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1]),
+        new Tour(3, [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1])
+    ];
+    const teamData2 = new TeamData(59865, 'Обниматься', 'Санкт-Петербург');
+    teamData2.tours = [
+        new Tour(1, [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0]),
+        new Tour(2, [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0]),
+        new Tour(3, [1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0]),
+    ];
+    return expect(parser.parse(new FileMock)).resolves.toEqual([teamData1, teamData2]);
 });
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,3 +1,8 @@
+window.requestAnimationFrame = function (callback: Function) {
+    setTimeout(callback, 0);
+    return 0;
+};
+
 import { configure } from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-16';
 
