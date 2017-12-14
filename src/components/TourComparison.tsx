eb9a6f92e279815eac3b80a8fac0ac1021620319
atanana <tanana@email.ru>
2017-12-14 22:24:55 +0300

    tour comparison ui init

--- src/components/TourComparison.tsx
+++ src/components/TourComparison.tsx
@@ -1,22 +1,42 @@
 import * as React from 'react';
 import { TeamData } from '../data/ResultsData';
 import { teamText } from '../utils/UiUtils';
+import * as _ from 'lodash';
 
 interface Props {
     teams: TeamData[];
 }
 
-const TourComparison = ({ teams }: Props) => (
-    <table className="table is-fullwidth is-hoverable is-striped">
-        <thead>
-        <th/>
-        {
-            teams.map(team => (
-                <th>{teamText(team)}</th>
-            ))
-        }
-        </thead>
-    </table>
-);
+// interface TourProps {
+//     tours: Tour[];
+// }
+//
+// const TourView = ({ tours }: TourProps) => (
+//     <>
+//     <tr key={`tour_${tour}`}>Тур {tour}</tr>
+//     </>
+// );
+
+const TourComparison = ({ teams }: Props) => {
+    const toursCount = teams[0].tours.length;
+    return (
+        <table className="table is-fullwidth is-hoverable is-striped">
+            <thead>
+            <th/>
+            {
+                teams.map(team => (
+                    <th key={team.id}>{teamText(team)}</th>
+                ))
+            }
+            </thead>
+            <tbody>
+            {
+                _.range(toursCount)
+                    .map(tour => (<tr key={`tour_${tour}`}>Тур {tour}</tr>))
+            }
+            </tbody>
+        </table>
+    );
+};
 
 export default TourComparison;
