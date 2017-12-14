d53cba66a5ec8a66f9e841f17d89b6678a8d73d6
atanana <tanana@email.ru>
2017-12-15 00:18:47 +0300

    tour comparison ui init

--- src/components/TourComparison.tsx
+++ src/components/TourComparison.tsx
@@ -1,38 +1,79 @@
 import * as React from 'react';
-import { TeamData } from '../data/ResultsData';
+import { Fragment } from 'react';
+import { TeamData, Tour } from '../data/ResultsData';
 import { teamText } from '../utils/UiUtils';
+
 import * as _ from 'lodash';
 
-interface Props {
+interface ComponentProps {
     teams: TeamData[];
 }
 
-// interface TourProps {
-//     tours: Tour[];
-// }
-//
-// const TourView = ({ tours }: TourProps) => (
-//     <>
-//     <tr key={`tour_${tour}`}>Тур {tour}</tr>
-//     </>
-// );
+interface TourProps {
+    tours: Tour[];
+}
 
-const TourComparison = ({ teams }: Props) => {
+interface AnswersProps {
+    answers: number[];
+    keyPrefix: string;
+}
+
+const AnswersRow = ({answers, keyPrefix}: AnswersProps) => (
+    <>
+    {
+        answers.map((answer, i) => (
+            <td key={`${keyPrefix}_${i}`}>{answer}</td>
+        ))
+    }
+    </>
+);
+
+const TourView = ({tours}: TourProps) => {
+    const tour = tours[0].tour;
+    const answers = tours[0].answers.length;
+    return (
+        <>
+        <tr>
+            <td colSpan={100} className="has-text-centered">Тур {tour}</td>
+        </tr>
+        {
+            _.range(answers)
+                .map(answer => (
+                    <Fragment key={`answer_${tour}_${answer}`}>
+                        <tr>
+                            <td>{answer + 1}</td>
+                            <AnswersRow
+                                keyPrefix={`answer_${tour}_${answer}`}
+                                answers={tours.map(item => item.answers[answer])}
+                            />
+                        </tr>
+                    </Fragment>
+                ))
+        }
+        </>
+    );
+};
+
+const TourComparison = ({teams}: ComponentProps) => {
     const toursCount = teams[0].tours.length;
     return (
         <table className="table is-fullwidth is-hoverable is-striped">
             <thead>
-            <th/>
-            {
-                teams.map(team => (
-                    <th key={team.id}>{teamText(team)}</th>
-                ))
-            }
+            <tr>
+                <th/>
+                {
+                    teams.map(team => (
+                        <th key={team.id}>{teamText(team)}</th>
+                    ))
+                }
+            </tr>
             </thead>
             <tbody>
             {
                 _.range(toursCount)
-                    .map(tour => (<tr key={`tour_${tour}`}>Тур {tour}</tr>))
+                    .map(tour => (
+                        <TourView key={`tour_${tour}`} tours={teams.map(team => team.tours[tour])}/>
+                    ))
             }
             </tbody>
         </table>
